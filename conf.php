4f7bb12ab08c73a7ec3ef5b16b4ce1e31839393f
an-rohangovekar <rohan.g@applicationnexus.com>
2016-04-26 13:38:33 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 
-	define('SHOPIFY_SHOP', 'your-store.myshopify.com');
-	define('SHOPIFY_APP_API_KEY', '');
-	define('SHOPIFY_APP_PASSWORD', '');
+	define('SHOPIFY_SHOP', 'https://littlewords-2.myshopify.com');
+	define('SHOPIFY_APP_API_KEY', 'bc586812d0180a13d8717e06bcdc0cae');
+	define('SHOPIFY_APP_PASSWORD', '3021e046fff907d1d9b4c3649b4ce869');
 
 ?>
